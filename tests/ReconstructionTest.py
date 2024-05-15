70191acc2fc416841a884c83d2bfca556cc99fd6
truzmeto <talantruzmetov@gmail.com>
2024-05-14 21:19:21 -0700

    unit test improved

--- tests/ReconstructionTest.py
+++ tests/ReconstructionTest.py
@@ -9,34 +9,35 @@ from MDAnalysis.analysis.bat import BAT
 
 class TesReconstruction(unittest.TestCase):
 
-    def test(self):
+    def setUp(self):
+
         data_path = '/home/talant/Projects/UCRiver/DeepLearning/AB13/TRAJ/AB13_1000.dcd'
         prmtop_path = '/home/talant/Projects/UCRiver/DeepLearning/AB13/TRAJ/AB13.prmtop'
-        device = 'cpu'
-
-    
-        u = mda.Universe(prmtop_path, data_path)
-        selected = u.select_atoms("protein")
+        self.device = 'cpu'
+        self.u = mda.Universe(prmtop_path, data_path)
+        selected = self.u.select_atoms("protein")
         R = BAT(selected)
         n_atoms = R.atoms.n_atoms
-         
-         
-        xyz = []
-        for i in u._trajectory:
-            xyz.append(i.positions)
-        xyz = np.array(xyz)
-        xyz = torch.as_tensor(xyz, device=device, dtype=torch.float32)
 
         tor_indxs = R._torsion_XYZ_inds
-        root_XYZ_inds = R._root_XYZ_inds
-
-        B = BATmain(n_atoms, tor_indxs, root_XYZ_inds , device=device)    
-        root_3_xyz = xyz[:,root_XYZ_inds,:]
+        self.root_XYZ_inds = R._root_XYZ_inds
+        self.B = BATmain(n_atoms, tor_indxs, self.root_XYZ_inds , device=self.device)    
+  
         
-        n1, n2, va, vb = B.Coords2MainVecs(xyz)
-        bonds, angles, torsions = B.BondAngleTorsion(n1, n2, va, vb)
         
-        xyz_new = B.BatV2Coords(bonds, angles, torsions, root_3_xyz)
+    def test(self):
+         
+        xyz = []
+        for i in self.u._trajectory:
+            xyz.append(i.positions)
+        xyz = np.array(xyz)
+        xyz = torch.as_tensor(xyz, device=self.device, dtype=torch.float32)
+        root_3_xyz = xyz[:,self.root_XYZ_inds,:]
+        
+        n1, n2, va, vb = self.B.Coords2MainVecs(xyz)
+        bonds, angles, torsions = self.B.BondAngleTorsion(n1, n2, va, vb)
+        
+        xyz_new = self.B.BatV2Coords(bonds, angles, torsions, root_3_xyz)
         rmsd = (xyz - xyz_new).pow(2).mean().sqrt()
         
         self.assertTrue( rmsd.item() < 0.001 )
